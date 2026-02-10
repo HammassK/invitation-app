d5461ea2f1097a019dde99ed3cf70c51ece0e119
HammassK <hammas.khan@agiledistrict.com>
2026-02-10 15:00:53 +0500

    Updates

---
+++ invitation-app/_next/static/E7dGhMUO-nazFL68ffnqA/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/invitation-app//_next/:path+",
+        "destination": "/invitation-app/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
