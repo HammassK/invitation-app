afe36adc1cbadf31f3930fa3af3e7976c8a0a58b
HammassK <hammas.khan@agiledistrict.com>
2026-02-10 14:51:28 +0500

    Updates

--- _next/static/DntCK_TovtGtfriMxMfDT/_buildManifest.js
+++ _next/static/DntCK_TovtGtfriMxMfDT/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/invitation-app//_next/:path+",
+        "destination": "/invitation-app/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
