d8170c98bd01c9e5d8e05684dc8c15bb14c48c4a
HammassK <hammas.khan@agiledistrict.com>
2026-02-10 14:52:00 +0500

    feat: deploy on github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/invitation-app",
+  assetPrefix: "/invitation-app/",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
