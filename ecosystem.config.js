c717345fd867a3cd0398176e0e37ce7b3fdf5272
HammassK <hammas.khan@agiledistrict.com>
2026-02-13 14:51:08 +0500

    chore: add ecosystem config file

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  apps: [
+    {
+      name: "invitation",
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
+      instances: 1,
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3003,
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 3003,
+      },
+      env_development: {
+        NODE_ENV: "development",
+        PORT: 3003,
+      },
+      error_file: "./logs/err.log",
+      out_file: "./logs/out.log",
+      log_file: "./logs/combined.log",
+      time: true,
+      autorestart: true,
+      max_restarts: 10,
+      min_uptime: "10s",
+      max_memory_restart: "1G",
+      watch: false,
+    },
+  ],
+};
